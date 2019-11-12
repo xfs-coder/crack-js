c66d8d066fa0069ab495a993876ba0cdbb594dd3
xfs-coder <56686860+xfs-coder@users.noreply.github.com>
2019-11-12 20:24:20 +0800

    Add files via upload

---
+++ 锦江wehotel/test.py
@@ -0,0 +1,15 @@
+# coding='utf-8'
+
+
+import execjs
+
+
+def main():
+	with open('test.js', 'r') as f:
+		ctx = execjs.compile(f.read(), cwd=r'E:\nodejs\node_global\node_modules')
+		black_box = ctx.call('black_box')
+		print(black_box)
+		
+
+if __name__ == '__main__':
+	main()
